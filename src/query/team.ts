c80876f8ede21499570428a9ad07c4ee89c10163
Joao Medeiros <joaoserj@gmail.com>
2020-01-31 14:39:21 -0300

    added method list all members

--- src/query/team.ts
+++ src/query/team.ts
@@ -1,5 +1,28 @@
 import { Response } from './response'
-
+import * as I from '../index'
+import * as _ from 'lodash'
 export class Team extends Response {
 
+  /**
+   * @description List all team members
+   * @param userId User ID
+   * @param teamIdFk Team ID
+   */
+  public async listAllMembers (userId: number, teamIdFk: number): Promise<any> {
+    try {
+      const team: I.Team[] = await this.api('teams')
+        .where({ id: teamIdFk, managerIdFk: userId }).select()
+
+      if (_.isNil(team)) {
+        throw new Error('Esse time não existe')
+      } else {
+        const listAllMembers = await this.api('team_rules').where({
+          teamIdFk: teamIdFk
+        }).join('users', 'users.id', 'team_rules.userIdFk').select('users.name', 'team_rules.id')
+        return listAllMembers
+      }
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
 }
