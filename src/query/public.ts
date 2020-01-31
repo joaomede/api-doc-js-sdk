9b168187f0b9510ffaca243864257d126ca37f67
Joao Medeiros <joaoserj@gmail.com>
2020-01-31 15:18:54 -0300

    added method get path and response public

--- src/query/public.ts
+++ src/query/public.ts
@@ -3,6 +3,22 @@ import * as I from '../index'
 
 export default class Public extends Team {
 
+  public async getPathAndResponsesPublic (tagId: number): Promise<void> {
+    try {
+      const verbAndCodes = await this.populate(this.knex, 'paths')
+        .find({ tagsIdFk: tagId })
+        .populate('responses', 'pathsIdFk', 'responses')
+        .exec()
+
+      if (verbAndCodes.length === 0) {
+        throw new Error('Não há verbos disponíveis')
+      } else {
+        return verbAndCodes
+      }
+    } catch (error) {
+      throw new Error('Erro ao tentar expandir')
+    }
+  }
 
   public async listAllPublicApi (): Promise<void> {
     try {
