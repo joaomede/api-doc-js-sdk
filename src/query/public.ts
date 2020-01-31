2875e595c72eaaeb5798326b47a6f8d76ef1bbaa
Joao Medeiros <joaoserj@gmail.com>
2020-01-31 15:19:12 -0300

    addded get api and tags public

--- src/query/public.ts
+++ src/query/public.ts
@@ -2,6 +2,25 @@ import { Team } from './team'
 import * as I from '../index'
 
 export default class Public extends Team {
+  public async getApiAndTagsPublic (apiId: number): Promise<void> {
+    try {
+      const api = await this.api('api').where({ id: apiId })
+      const tags: I.Tag[] = await this.api('tags').where({ apiIdFk: apiId })
+      api[0].tags = tags
+
+      if (api.length === 0) {
+        throw new Error('A api que você está tentando acessar não foi encontrada')
+      } else {
+        if (api[0].isPublic === 1 || api[0].isPublic === true) {
+          return api[0]
+        } else {
+          throw new Error('Você não tem autorização para acessar essa documentação')
+        }
+      }
+    } catch (error) {
+      throw new Error('Erro ao tentar carregar a documentação')
+    }
+  }
 
   public async getPathAndResponsesPublic (tagId: number): Promise<void> {
     try {
