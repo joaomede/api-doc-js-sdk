247c07b1ef69c2f34364427ad41bf3f702c1f7a1
Joao Medeiros <joaoserj@gmail.com>
2020-01-31 15:18:30 -0300

    added method list all public api

--- src/query/public.ts
+++ src/query/public.ts
@@ -1,5 +1,18 @@
 import { Team } from './team'
+import * as I from '../index'
 
 export default class Public extends Team {
 
+
+  public async listAllPublicApi (): Promise<void> {
+    try {
+      const allPublicList = await this.api('api')
+        .where({ isPublic: true })
+        .join('users', 'users.id', 'api.userIdFk')
+        .select('api.id', 'api.descriptionApi', 'api.apiName', 'users.name')
+      return allPublicList
+    } catch (error) {
+      throw new Error('Erro ao tentar listar documentação')
+    }
+  }
 }
