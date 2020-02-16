05d746226b6d2c3ef7cc78f0084e326fa530319d
joao <joaoserj@gmail.com>
2020-02-16 09:18:49 -0300

    fix migration

--- src/migrations/09_add_query_to_paths.ts
+++ src/migrations/09_add_query_to_paths.ts
@@ -1,7 +1,7 @@
 import * as Knex from 'knex'
 
 export async function up (knex: Knex): Promise<void> {
-  return knex.schema.createTable('paths', table => {
+  return knex.schema.table('paths', table => {
     table.json('query').notNullable()
   })
 }
